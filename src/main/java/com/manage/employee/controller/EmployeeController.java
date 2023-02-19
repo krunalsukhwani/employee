9b85bbd1f087f35c26b4da7277bc9db1b104bd6b
krunalsukhwani <krunal.sukhwani@gmail.com>
2023-02-18 20:07:03 -0500

    View Operation

--- src/main/java/com/manage/employee/controller/EmployeeController.java
+++ src/main/java/com/manage/employee/controller/EmployeeController.java
@@ -4,8 +4,10 @@ import com.manage.employee.model.Employee;
 import com.manage.employee.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class EmployeeController {
@@ -13,14 +15,21 @@ public class EmployeeController {
     @Autowired
     private EmployeeRepository employeeRepository;
 
+    @RequestMapping("/")
+    public String getIndexPage(Model model){
+        model.addAttribute("employees", employeeRepository.findAll());
+        return "index";
+    }
+
     @GetMapping("/add")
     public String getAddEmployeeForm(Employee employee){
         return "add-employee";
     }
 
     @PostMapping("/add")
-    public String insertEmployeeData(Employee employee){
+    public String insertEmployeeData(Employee employee,Model model){
         employeeRepository.save(employee);
-        return "success";
+        model.addAttribute("employees", employeeRepository.findAll());
+        return "index";
     }
 }
