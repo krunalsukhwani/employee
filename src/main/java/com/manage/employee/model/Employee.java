b96f5582e97fa022edca813da6afbb8b5f08061b
krunalsukhwani <krunal.sukhwani@gmail.com>
2023-02-18 21:56:44 -0500

    added lombok

--- src/main/java/com/manage/employee/model/Employee.java
+++ src/main/java/com/manage/employee/model/Employee.java
@@ -3,11 +3,16 @@ package com.manage.employee.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "employee")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Employee {
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
@@ -20,50 +25,4 @@ public class Employee {
 
     @Min(value = 0,message = "Salary should be greater than zero.")
     private double salary;
-
-    //No arg constructor
-    public Employee() {
-    }
-
-    //All arg constructor
-    public Employee(int id, String name, String address, double salary) {
-        this.id = id;
-        this.name = name;
-        this.address = address;
-        this.salary = salary;
-    }
-
-
-    //getter and setter methods
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
 }
